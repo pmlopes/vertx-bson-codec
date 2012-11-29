2d46b9de982c118332ff167eaf5fa4ea15e8b68c
Paulo Lopes <pmlopes@gmail.com>
2012-11-29 16:52:43 +0100

    add real tests based on the minimal examples from bsonspec.org

--- test/bson/vertx/eventbus/BSONCodecTest.java
+++ test/bson/vertx/eventbus/BSONCodecTest.java
@@ -1,21 +1,218 @@
 package bson.vertx.eventbus;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
+import org.vertx.java.core.buffer.Buffer;
 
 import java.util.*;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
 public class BSONCodecTest {
 
-    @BeforeClass
-    public static void testSetup() {
-        // Preparation of the unit tests
+    @Test
+    public void testEncodeBSONSPEC1() {
+        Map<String, Object> json = new HashMap<>();
+        json.put("hello", "world");
+
+        // this is the hello world example from http://bsonspec.org
+        byte[] expected = new byte[]{
+                (byte) 0x16,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x02,
+                'h',
+                'e',
+                'l',
+                'l',
+                'o',
+                (byte) 0x00,
+                (byte) 0x06,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                'w',
+                'o',
+                'r',
+                'l',
+                'd',
+                (byte) 0x00,
+                (byte) 0x00
+        };
+
+        byte[] bson = BSONCodec.encode(json).getBytes();
+
+        assertArrayEquals(expected, bson);
     }
 
-    @AfterClass
-    public static void testCleanup() {
-        // Teardown for data used by the unit tests
+    @Test
+    public void testDecodeBSONSPEC1() {
+        // this is the hello world example from http://bsonspec.org
+        byte[] bson = new byte[]{
+                (byte) 0x16,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x02,
+                'h',
+                'e',
+                'l',
+                'l',
+                'o',
+                (byte) 0x00,
+                (byte) 0x06,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                'w',
+                'o',
+                'r',
+                'l',
+                'd',
+                (byte) 0x00,
+                (byte) 0x00
+        };
+
+        Map json = BSONCodec.decode(new Buffer(bson));
+
+        Map<String, String> expected = new HashMap<>();
+        expected.put("hello", "world");
+
+        assertEquals(expected, json);
+    }
+
+    @Test
+    public void testEncodeBSONSPEC2() {
+        // this is {"BSON": ["awesome", 5.05, 1986]} from http://bsonspec.org
+        Map<String, Object> json = new HashMap<>();
+        List<Object> list = new ArrayList<>();
+        list.add("awesome");
+        list.add(5.05);
+        list.add(1986);
+        json.put("BSON", list);
+
+        byte[] expected = new byte[]{
+                (byte) 0x31,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x04,
+                'B',
+                'S',
+                'O',
+                'N',
+                (byte) 0x00,
+                (byte) 0x26,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x02,
+                '0',
+                (byte) 0x00,
+                (byte) 0x08,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                'a',
+                'w',
+                'e',
+                's',
+                'o',
+                'm',
+                'e',
+                (byte) 0x00,
+                (byte) 0x01,
+                '1',
+                (byte) 0x00,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x14,
+                (byte) 0x40,
+                (byte) 0x10,
+                '2',
+                (byte) 0x00,
+                (byte) 0xc2,
+                (byte) 0x07,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00
+        };
+
+        byte[] bson = BSONCodec.encode(json).getBytes();
+
+        assertArrayEquals(expected, bson);
+    }
+
+    @Test
+    public void testDecodeBSONSPEC2() {
+        byte[] bson = new byte[]{
+                (byte) 0x31,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x04,
+                'B',
+                'S',
+                'O',
+                'N',
+                (byte) 0x00,
+                (byte) 0x26,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x02,
+                '0',
+                (byte) 0x00,
+                (byte) 0x08,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                'a',
+                'w',
+                'e',
+                's',
+                'o',
+                'm',
+                'e',
+                (byte) 0x00,
+                (byte) 0x01,
+                '1',
+                (byte) 0x00,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x33,
+                (byte) 0x14,
+                (byte) 0x40,
+                (byte) 0x10,
+                '2',
+                (byte) 0x00,
+                (byte) 0xc2,
+                (byte) 0x07,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00,
+                (byte) 0x00
+        };
+
+        Map<String, Object> expected = new HashMap<>();
+        List<Object> list = new ArrayList<>();
+        list.add("awesome");
+        list.add(5.05);
+        list.add(1986);
+        expected.put("BSON", list);
+
+        Map json = BSONCodec.decode(new Buffer(bson));
+
+        assertEquals(expected, json);
     }
 
     @Test
