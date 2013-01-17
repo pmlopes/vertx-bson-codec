c846f30e0400cd29948740229f201ec989275876
Paulo Lopes <plopes@roughcookie.com>
2013-01-17 16:17:18 +0100

    Integration tests now fail if assertions are not met

--- src/vertxInteg/java/com/jetdrone/vertx/bson/ModuleIntegrationTest.java
+++ src/vertxInteg/java/com/jetdrone/vertx/bson/ModuleIntegrationTest.java
@@ -1,5 +1,6 @@
 package com.jetdrone.vertx.bson;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.vertx.java.core.eventbus.EventBus;
@@ -12,17 +13,32 @@ import org.vertx.java.core.Handler;
 import org.vertx.java.core.eventbus.Message;
 import org.vertx.java.test.VertxTestBase;
 import org.vertx.java.test.junit.VertxJUnit4ClassRunner;
+import org.vertx.java.test.utils.QueueReplyHandler;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 @RunWith(VertxJUnit4ClassRunner.class)
 @VertxConfiguration
 public class ModuleIntegrationTest extends VertxTestBase {
+
+    private long timeout = 2;
+
+    @Before
+    public void setup() {
+        this.timeout = Long.parseLong(System.getProperty("vertx.test.timeout", "2"));
+    }
+
     @Test
     public void testSimpleTimes2HandlerOverBSON() {
+
+        final LinkedBlockingQueue<Map> queue = new LinkedBlockingQueue<>();
+
         BSONEventBus bsonEventBus = new BSONEventBus(getVertx());
 
         bsonEventBus.registerHandler("bson.times2.handler", new Handler<Message<Map>>() {
@@ -40,19 +56,56 @@ public class ModuleIntegrationTest extends VertxTestBase {
         Map<String, Integer> msg = new HashMap<>();
         msg.put("value", 5);
 
-        bsonEventBus.send("bson.times2.handler", msg, new Handler<Message<Map>>() {
+        bsonEventBus.send("bson.times2.handler", msg, new QueueReplyHandler<Map>(queue, timeout));
+
+        try {
+            Map answer = queue.poll(timeout, TimeUnit.SECONDS);
+            assertEquals(10, answer.get("value"));
+        } catch (InterruptedException e) {
+            fail(e.getMessage());
+        }
+    }
+
+    @Test
+    public void testSimpleTimes2LocalHandlerOverBSON() {
+
+        final LinkedBlockingQueue<Map> queue = new LinkedBlockingQueue<>();
+
+        BSONEventBus bsonEventBus = new BSONEventBus(getVertx());
+
+        bsonEventBus.registerLocalHandler("bson.local.times2.handler", new Handler<Message<Map>>() {
             @Override
             public void handle(Message<Map> mapMessage) {
-                assertEquals(10, mapMessage.body.get("value"));
+                assertEquals(5, mapMessage.body.get("value"));
+
+                Map<String, Integer> replyMsg = new HashMap<>();
+                replyMsg.put("value", ((Integer) mapMessage.body.get("value")) * 2);
+
+                mapMessage.reply(replyMsg);
             }
         });
+
+        Map<String, Integer> msg = new HashMap<>();
+        msg.put("value", 5);
+
+        bsonEventBus.send("bson.local.times2.handler", msg, new QueueReplyHandler<Map>(queue, timeout));
+
+        try {
+            Map answer = queue.poll(timeout, TimeUnit.SECONDS);
+            assertEquals(10, answer.get("value"));
+        } catch (InterruptedException e) {
+            fail(e.getMessage());
+        }
     }
 
     @Test
     public void testSimpleTimes2HandlerOverJSON() {
+
+        final LinkedBlockingQueue<JsonObject> queue = new LinkedBlockingQueue<>();
+
         EventBus eventBus = getVertx().eventBus();
 
-        eventBus.registerHandler("bson.times2.handler", new Handler<Message<JsonObject>>() {
+        eventBus.registerHandler("json.times2.handler", new Handler<Message<JsonObject>>() {
             @Override
             public void handle(Message<JsonObject> jsonMessage) {
                 assertEquals(5,  (int) jsonMessage.body.getInteger("value"));
@@ -67,11 +120,13 @@ public class ModuleIntegrationTest extends VertxTestBase {
         JsonObject msg = new JsonObject();
         msg.putNumber("value", 5);
 
-        eventBus.send("bson.times2.handler", msg, new Handler<Message<JsonObject>>() {
-            @Override
-            public void handle(Message<JsonObject> mapMessage) {
-                assertEquals(10,  (int) mapMessage.body.getInteger("value"));
-            }
-        });
+        eventBus.send("json.times2.handler", msg, new QueueReplyHandler<JsonObject>(queue, timeout));
+
+        try {
+            JsonObject answer = queue.poll(timeout, TimeUnit.SECONDS);
+            assertEquals(10, (int) answer.getInteger("value"));
+        } catch (InterruptedException e) {
+            fail(e.getMessage());
+        }
     }
 }
